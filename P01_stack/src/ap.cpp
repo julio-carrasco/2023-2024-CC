37f46526083b1ba419dd0f2fd0bff8c572f1e4d1
Julio Carrasco <alu0101110639@ull.edu.es>
2023-10-02 21:39:21 +0000

    a

--- P01_stack/src/ap.cpp
+++ P01_stack/src/ap.cpp
@@ -4,19 +4,6 @@
 // Complejidad de computacion
 // alu0101110639@ull.edu.es
 
-
-#include "../include/ap.h"
-
-
-void Stack_machine::transition(std::string input) {
-  
-}
-// Julio Ivan Carrasco Armas
-// 4º Ingenieria Informatica
-// Universidad de La Laguna
-// Complejidad de computacion
-// alu0101110639@ull.edu.es
-
 #include "../include/ap.h"
 
 /**
